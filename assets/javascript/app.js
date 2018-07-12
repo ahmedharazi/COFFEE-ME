06b73bec3f83c44f6d892fcff97a9124b4c140a8
Adam Yab <ahmed.harazi22@gmail.com>
2018-07-12 18:22:12 -0400

    the fianal touch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,3 @@
-
-
 //global scope
 var map;
 var infowindow;
@@ -9,52 +7,90 @@ var service;
 var markers = [];
 
 function initialize() {
-    var center = new google.maps.LatLng(28.5383, -81.3792);
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: center,
-        zoom: 13
-    });
-
-    request = {
-        location: center,
-        // in meter (5miles)
-        radius: 8047,
-        types: ['cafe']
-    };
-    // opens info window in google map 
-    infowindow = new google.maps.InfoWindow();
-    service = new google.maps.places.PlacesService(map);
-
-    service.nearbySearch(request, callback);
-
-    // listen for click on map and take that to look for other coffe shops in that location 
-    google.maps.event.addListener(map, 'rightclick', function (event) {
-        map.setCenter(event.latLng)
-        clearResults(markers)
+    navigator.geolocation.getCurrentPosition(function (position) {
+        var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        // var center = new google.maps.LatLng(28.5383, -81.3792);
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: pos,
+            zoom: 13
+        });
+        service = new google.maps.places.PlacesService(map);
 
         request = {
-            location: event.latLng,
+            location: pos,
+            // in meter (5miles)
             radius: 8047,
             types: ['cafe']
         };
+        // opens info window in google map 
+        infowindow = new google.maps.InfoWindow();
+
         service.nearbySearch(request, callback);
-    })
+
+        // listen for click on map and take that to look for other coffe shops in that location 
+        google.maps.event.addListener(map, 'rightclick', function (event) {
+            map.setCenter(event.latLng)
+            clearResults(markers)
+
+            request = {
+                location: event.latLng,
+                radius: 8047,
+                types: ['cafe']
+            };
+            console.log(request);
+            service.nearbySearch(request, callback);
+            console.log(callback);
+        })
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                };
+
+                infowindow.setPosition(pos);
+                infowindow.setContent('Current Location.');
+                infowindow.open(map);
+                map.setCenter(pos);
+            }, function () {
+                handleLocationError(true, infowindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infowindow, map.getCenter());
+        }
+    });
+
+    function handleLocationError(browserHasGeolocation, infowindow, pos) {
+        infowindow.setPosition(pos);
+        infowindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+        infowindow.open(map);
+    }
 }
+
+
+//service.nearbySearch(request, callback);
 
 // get back good result, no error connection to server
 
 function callback(results, status) {
-    console.log(status);
-    console.log(results);
+
     if (status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
+        var data = results.slice(0, 10);
+
+        for (var i = 0; i < data.length; i++) {
             console.log(results);
             // var place = results[i];
             var rate = JSON.stringify(results[i].rating);
             var rating = $("<div>");
             rating.text(rate);
             $("#rating").append(rating);
-            console.log(rate);
+
             var geo = JSON.stringify(results[i].vicinity);
             var location = $("<div>");
             location.text(geo);
@@ -63,7 +99,7 @@ function callback(results, status) {
             var namely = $("<div>");
             namely.text(name);
             $("#name").append(namely);
-            
+
             markers.push(createMarker(results[i]));
             // console.log(markers);
         }
@@ -97,19 +133,22 @@ function clearResults(markers) {
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 
-$("#search").on("click", function(event){
+$("#search").on("click", function (event) {
     event.initialize();
 })
 //$("#map").append(map);
 
 
+
+
+
 function Headline() {
     $.ajax({
-        url: "https://api.nytimes.com/svc/topstories/v2/home.json?"
-            + "&api-key=72e2a44892c743248b362965fbe0d583&limit=10",
+        url: "https://api.nytimes.com/svc/topstories/v2/home.json?" +
+            "&api-key=72e2a44892c743248b362965fbe0d583&limit=10",
         method: "GET"
     }).then(function (response) {
-        var news = response.results.slice(0, 5);
+        var news = response.results.slice(0, 3);
 
         // console.log(news);
         for (var i = 0; i < news.length; i++) {
